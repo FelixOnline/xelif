869c604b32b65f5583f4a4392b463ddc9a75d0ff
Andy Wang <cbeuw.andy@gmail.com>
2021-01-04 15:49:43 +0000

    Let article opening attention play well with HTML character references (such as &nbsp;)
    Signed-off-by: Andy Wang <cbeuw.andy@gmail.com>

--- resources/views/blocks/text.blade.php
+++ resources/views/blocks/text.blade.php
@@ -10,9 +10,9 @@ $bringAttention = function($inputHtml, $look) {
     // html regex parse go brrrrr
     $paras = explode("</p>", $inputHtml, 1);
     $firstParaOpenPos = stripos($paras[0], "<p>");
-    $firstParaText = $firstParaOpenPos === FALSE
-                   ? $paras[0]
-                   : substr($paras[0], $firstParaOpenPos + 3);
+    $firstParaText = html_entity_decode($firstParaOpenPos === FALSE
+                                       ? $paras[0]
+                                       : substr($paras[0], $firstParaOpenPos + 3));
 
     $firstPuncPos = strcspn($firstParaText, $punctuationMarks);
 
