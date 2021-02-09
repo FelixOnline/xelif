7a97eefac69b53309579129219b1cf0dfc2f5ba0
Andy Wang <cbeuw.andy@gmail.com>
2021-02-09 00:50:45 +0000

    Display publication date in by-line of articles
    Signed-off-by: Andy Wang <cbeuw.andy@gmail.com>

--- resources/views/components/byline.blade.php
+++ resources/views/components/byline.blade.php
@@ -1,4 +1,8 @@
 @props(['article'])
 <p class="byline">by
 <x-name-list :writers="$article->writers" />
+<br>
+@if ($article->publish_start_date)
+    <span>on {{ date('j F Y',strtotime($article->publish_start_date)) }}</span>
+@endif
 </p>
